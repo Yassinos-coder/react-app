b052e3cd20eeec8190755ef08f84024635026141
Yassinos-Coder <castroyassine0@gmail.com>
2022-09-28 22:27:15 +0100

    Chnged bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Signup from './components/signup';
 import './css/App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="app">
-      <Signup />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/App.css";
+
+const Header = () => {
+  return (
+   <div className="container">
+        <div className="title">
+            <h2>Apply for a seat at the table <br /> Below </h2>
+        </div>
+   </div>
+  );
+};
+export default Header;
